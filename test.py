3ccd4883fe5ff484cab01d9700bb34832762c849
charlesnoah344 <23397@ecam.be>
2026-01-25 22:18:41 +0100

    test de la fonction playe verts

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-import tictactoe as ttt
+import pytest
+from tictactoe import  initial_state, player
+
+EMPTY = None
+X = "X"
+O = "O"
+
+def test_initial_state():
+    assert initial_state() == [[EMPTY, EMPTY, EMPTY],
+                               [EMPTY, EMPTY, EMPTY],
+                               [EMPTY, EMPTY, EMPTY]]
+    
+def test_player():
+    assert player([[EMPTY, EMPTY, EMPTY],
+                   [EMPTY, EMPTY, EMPTY],
+                   [EMPTY, EMPTY, EMPTY]]) == X
+    
+    assert player([[EMPTY, EMPTY, EMPTY],
+                   [EMPTY, X, EMPTY],
+                   [EMPTY, EMPTY, EMPTY]]) == O
+    
+    assert player([[EMPTY, EMPTY, EMPTY],
+                   [EMPTY, X, EMPTY],
+                   [EMPTY, EMPTY, O]]) == X
